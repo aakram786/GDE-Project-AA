4422e8c6b385d6817086cdca1ccf27b39fc7a7b3
Anwar Akram <anwar.s.akram@gmail.com>
2023-09-06 00:03:37 +0100

    latest release

--- clean_environment.py
+++ clean_environment.py
@@ -5,8 +5,8 @@ import requests
 del1=requests.delete('http://127.0.0.1:5000/users/1')
 del2=requests.delete('http://127.0.0.1:5000/users/2')
 
-print(f'Server for rest stopped {del1.status_code}')
-print(f'Server for app stopped {del2.status_code}')
+print(f'User 1 deleted stopped {del1.status_code}')
+print(f'User 2 deleted stopped {del2.status_code}')
 
 try:
     res1 = requests.get('http://127.0.0.1:5000/stop_server')
